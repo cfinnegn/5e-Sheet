928d362a7cb7bbe5fd47a2812c8f12a6d21a46f8
cfinnegn <cfinnegn@udel.edu>
2017-08-24 15:34:53 -0400

    Profline prefab/script added

---
+++ Assets/scripts/Prof_line.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prof_line : MonoBehaviour {
+
+	public GameObject profline;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Remove() {
+		GameObject.Destroy(this.gameObject);
+	}
+
+	public void Add(Transform parent) {
+		Instantiate(profline, parent);
+	}
+}
